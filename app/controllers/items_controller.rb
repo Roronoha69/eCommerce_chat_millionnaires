0af685501fc011da39af379dc5296e860ec7d81e
Roronoha69 <mbodje.justin@gmail.com>
2021-11-29 14:10:45 +0100

    controller item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def create
+    @item = Item.create(
+      title: Faker::Creature::Cat.name,
+      description: Faker::Lorem.sentences(number: 5),
+      price: Faker::Number.number(digits: 3)
+    )
+    
   end
+
+  private
+
+  def items_params
+    params.permit(:title, :description, :price)
 end
